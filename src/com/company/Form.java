b055ebce2a0326c2dadacfd8e41047e09cd5d911
Pavel <clownpavel@gmail.com>
2015-02-25 00:08:26 +0300

    Form

---
+++ src/com/company/Form.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by Pavel on 24.02.2015.
+ */
+public class Form {
+    private JTextField textField1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
+        // write your code here
         System.out.println("Hello world!");
         Encrypt enc = new Encrypt();
     }
