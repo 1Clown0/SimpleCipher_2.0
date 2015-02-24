35aeb86e57e851e2d5e9297039e37da6ad3f3e70
Pavel <clownpavel@gmail.com>
2015-02-24 23:49:44 +0300

    Comment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
 	// write your code here
         System.out.println("Hello world!");
-        Encrypt enc = new Encrypt();
+        //Encrypt enc = new Encrypt();
     }
 }
