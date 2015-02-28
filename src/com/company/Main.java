873734ef5ccdbf7b8016007728c99fe6fc70e2ed
Pavel <clownpavel@gmail.com>
2015-02-28 15:07:32 +0300

    Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
 package com.company;
 
+import com.company.GUI.Form;
+
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // write your code here
-        System.out.println("Hello world!");
-        Encrypt enc = new Encrypt();
+        new Form();
     }
 }
