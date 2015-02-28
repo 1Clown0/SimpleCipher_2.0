2e9b2256d6c910c893a1327a5d6c21b7ea657473
41group <alex-0102@yandex.ru>
2015-02-28 17:31:44 +0300

    Создание файла с тестами

---
+++ src/com/company/Test/TestCoding.java
@@ -0,0 +1,46 @@
+package com.company.Test;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Администратор
+ * Date: 28.02.15
+ * Time: 17:13
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(BlockJUnit4ClassRunner.class)
+
+public class TestCoding extends TestCase {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testEncryption() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDecryption() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetBytesFromFile() throws Exception {
+
+    }
+}
