63400e152b0222dbfea1e4a81326b325c7355bd8
41group <alex-0102@yandex.ru>
2015-03-17 23:42:14 +0300

    Написать юнит-тесты для методов класса Codding. #5

--- src/com/company/Test/TestCoding.java
+++ src/com/company/Test/TestCoding.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -37,7 +38,7 @@ public class TestCoding extends TestCase {
     @Test
     public void testCreateFileFullName() throws Exception {
         Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(decrypted).txt") == true);
+        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(decrypted)") == true);
     }
 
     @Test
@@ -49,43 +50,58 @@ public class TestCoding extends TestCase {
     @Test
     public void testNotCreateFileName() throws Exception {
         Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("(decrypted).txt") == false);
+        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("(decrypted)") == false);
     }
 
     @Test
     public void testNotCreateFileMode() throws Exception {
         Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test().txt") == false);
+        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test()") == false);
     }
 
     @Test
-    public void testNotCreateFileExtension() throws Exception {
+    public void testCreateFileExtension() throws Exception {
         Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(decrypted).") == false);
+        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(decrypted).txt") == false);
     }
 
     @Test
     public void testCreateInvalidFileName() throws Exception {
         Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("xxx(decrypted).txt") == false);
+        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("xxx(decrypted)") == false);
     }
 
     @Test
     public void testCreateInvalidFileMode() throws Exception {
         Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(xxx).txt") == false);
+        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(xxx)") == false);
     }
 
     @Test
-    public void testCreateInvalidFileExtension() throws Exception {
+    public void testCreateFileFullNameWithDot() throws Exception {
         Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(decrypted).xxx") == false);
+        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(decrypted).") == false);
     }
 
     @Test
-    public void testCreateFileFullNameWithoutDot() throws Exception {
-        Coding coding = new Coding();
-        assertTrue(coding.createFileName("test.txt", "decrypted").equalsIgnoreCase("test(decrypted)txt") == false);
+    public void testGetBytesFromFile() throws Exception {
+        File file = new File("testFile.txt");
+        file.createNewFile();
+        FileOutputStream out = new FileOutputStream(file);
+        out.write("abc".getBytes());
+        out.close();
+        assertTrue(new String(Coding.getBytesFromFile(file)).equals("abc") == true);
+        file.delete();
     }
 
+    @Test
+    public void testGetInvalidBytesFromFile() throws Exception {
+        File file = new File("testFile.txt");
+        file.createNewFile();
+        FileOutputStream out = new FileOutputStream(file);
+        out.write("abc".getBytes());
+        out.close();
+        assertTrue(new String(Coding.getBytesFromFile(file)).equals("xyz") == false);
+        file.delete();
+    }
 }
